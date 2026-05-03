231b4048c42ee74af3edb895295396d8f1a1628d
Rishi Sharma <rishikumarsharma434@gmail.com>
2026-05-03 18:17:25 +0530

    Configure app to run on specified port
    Updated app.run to use environment variable for port.

--- app.py
+++ app.py
@@ -397,4 +397,6 @@ def authorize_provider(provider):
     return redirect('/dashboard')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
